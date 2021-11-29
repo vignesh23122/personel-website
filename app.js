784af1ac1d495b8de6c219d4b3efa495012c8b0c
Vignesh <srivignesh23122001@gmail.com>
2021-11-29 21:22:01 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+
+//    rightclick prevention
+ document.querySelector('body').addEventListener('contextmenu',(e) => {
+ e.preventDefault();
+ })
+
+
+// create a play buttom
+
+//  var mysong = document.getElementById("mysong");
+//  var icon = document.getElementById("play-img");
+// var giffy = document.getElementById("giff");
+// var first = document.getElementById("first");
+// var set = document.getElementById("set");
+
+
+//  icon.onclick = function () {
+//      if (mysong.paused) {
+//          mysong.play();
+//          icon.src = "icon/icons8-pause-40.png";
+//         giffy.style.display = 'inline';
+//      }else{
+//         mysong.pause();
+//         icon.src = "icon/icons8-play-40.png";
+//        giffy.style.display = 'none';
+//      }
+     
+//  }
+
+//  onscrool to disable
+ 
+//  window.onscroll = function() {myFunction()};
+
+// function myFunction() {
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     mysong.pause();
+//     icon.src = "icon/icons8-play-40.png";
+//    giffy.style.display = 'none';
+//     // document.getElementById("myP").className = "test";
+//   } 
+// }
+
